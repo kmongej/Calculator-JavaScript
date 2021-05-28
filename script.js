6ea57f1d28a8b81d6638cf05b83b764f762f7018
kmongej <84548973+kmongej@users.noreply.github.com>
2021-05-27 19:50:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,28 +3,28 @@ function limpiar() {
 }
 
 function sumar() {
-    var x = parseInt(document.getElementById('valor1').value);
-    var y = parseInt(document.getElementById('valor2').value);
-    document.getElementById('el-resultado').innerHTML = x+y;
+    let x = parseInt(document.getElementById('valor1').value);
+    let y = parseInt(document.getElementById('valor2').value);
+    document.getElementById('el-resultado').innerHTML = x + y;
     
 }
 
 function restar() {
-    var x = parseInt(document.getElementById('valor1').value);
-    var y = parseInt(document.getElementById('valor2').value);
-    document.getElementById('el-resultado').innerHTML = x-y;
+    let x = parseInt(document.getElementById('valor1').value);
+    let y = parseInt(document.getElementById('valor2').value);
+    document.getElementById('el-resultado').innerHTML = x - y;
     
 }
 
 function multiplicar() {
-    var x = parseInt(document.getElementById('valor1').value);
-    var y = parseInt(document.getElementById('valor2').value);
-    document.getElementById('el-resultado').innerHTML = x*y;
+    let x = parseInt(document.getElementById('valor1').value);
+    let y = parseInt(document.getElementById('valor2').value);
+    document.getElementById('el-resultado').innerHTML = x * y;
     
 }
 function dividir() {
-    var x = parseInt(document.getElementById('valor1').value);
-    var y = parseInt(document.getElementById('valor2').value);
-    document.getElementById('el-resultado').innerHTML = x/y;
+    let x = parseInt(document.getElementById('valor1').value);
+    let y = parseInt(document.getElementById('valor2').value);
+    document.getElementById('el-resultado').innerHTML = x / y;
     
 }
